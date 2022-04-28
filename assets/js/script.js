55ea9d64f739446c273ff31d6a7b80914bda7703
franciscoklaus <franciscoklausfilho@hotmail.com>
2022-04-28 11:50:51 -0300

    criado script que ao escolher estado carrega as cidades dele

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+
+const selectStates = document.querySelector("#states");
+const selectCities = document.querySelector("#cities");
+function populatedStateSelect(){
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome').then(res => res.json()).then(states => {
+        states.map(state => {
+            const option = document.createElement('option');
+            option.setAttribute('value', state.sigla);
+            option.textContent = state.sigla;
+
+            selectStates.appendChild(option)
+        })
+    })
+}
+
+
+function populateCitySelect(){
+    selectStates.addEventListener('change', () =>{
+        let nodesSelectCities = selectCities.childNodes;
+        [...nodesSelectCities].map(node => node.remove());
+        let state = selectStates.options[selectStates.selectedIndex].value;
+        
+        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state}/municipios`).then(res => res.json()).then(cities => {
+            selectCities.removeAttribute('disabled');
+            cities.map(city => {
+                const option = document.createElement('option');
+                option.textContent = city.nome;
+                selectCities.appendChild(option);
+            })
+        })
+    })
+}
